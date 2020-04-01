61b04fdf10e06aa1ace97243be2717e68439158d
riya12e <riyatanuja36@gmail.com>
2020-04-01 02:22:02 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,19 +6,53 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  private id:string;
-  private name:string;
-  private salary:string;
-  private department:string;
+  id:number;
+  name:string;
+  salary:number;
+  dep:string;
+  employee=[{id:101,name:'riya',salary:5000,dep:'java'}];
   
-
   constructor(){
   }
-  getDetails(arg1:any,arg2:any,arg3:any,arg4:any){
-     this.id=(<HTMLInputElement>arg1).value;
-     this.name=(<HTMLInputElement>arg2).value;
-     this.salary=(<HTMLInputElement>arg3).value;
-     this.department=(<HTMLInputElement>arg4).value;
-     alert(this.id+" "+this.name+" "+this.salary+" "+this.department);
+  add(){
+    var emp={id:this.id,name:this.name,salary:this.salary,dep:this.dep};
+       this.employee.push(emp);
   }
+ delete(id:number){
+    var flag:boolean=false;
+   for (var i = 0;i <this.employee.length;i++) {
+      if (this.employee[i].id === id) {
+          flag=true;
+          this.employee.splice(i, 1);
+      }
+    }
+    return flag;
+  }
+newid:number;
+newname:string;
+newsalary:number;
+newdepartment:string;
+update(id:number,name:string,salary:number,dep:string)
+{
+  this.newid=id;
+  this.newname=name;
+  this.newsalary=salary;
+  this.newdepartment=dep;
+  }
+updateValues(id:number,name:string,salary:number,dep:string)
+{
+  for(var i=0;i<this.employee.length;i++){
+    let e=this.employee[i];
+    if(e.id==id){
+      //console.log(name);
+      this.employee.splice(i,1);  
+      this.employee.push({id,name,salary,dep});
+      //console.log(e.name);
+      break;
+    }
 }
+
+}
+}
+
+
