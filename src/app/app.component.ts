c3516f9a9e9a83381238b03ebd509d405b01ef03
riya12e <riyatanuja36@gmail.com>
2020-04-02 23:53:42 +0100

    3 files modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,53 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-  id:number;
-  name:string;
-  salary:number;
-  dep:string;
-  employee=[{id:101,name:'riya',salary:5000,dep:'java'}];
-  
-  constructor(){
-  }
-  add(){
-    var emp={id:this.id,name:this.name,salary:this.salary,dep:this.dep};
-       this.employee.push(emp);
-  }
- delete(id:number){
-    var flag:boolean=false;
-   for (var i = 0;i <this.employee.length;i++) {
-      if (this.employee[i].id === id) {
-          flag=true;
-          this.employee.splice(i, 1);
-      }
-    }
-    return flag;
-  }
-newid:number;
-newname:string;
-newsalary:number;
-newdepartment:string;
-update(id:number,name:string,salary:number,dep:string)
-{
-  this.newid=id;
-  this.newname=name;
-  this.newsalary=salary;
-  this.newdepartment=dep;
-  }
-updateValues(id:number,name:string,salary:number,dep:string)
-{
-  for(var i=0;i<this.employee.length;i++){
-    let e=this.employee[i];
-    if(e.id==id){
-      //console.log(name);
-      this.employee.splice(i,1);  
-      this.employee.push({id,name,salary,dep});
-      //console.log(e.name);
-      break;
-    }
-}
+   public pid:number;
+  public prodName:string;
+  public prodCost:number;
+  public prodOnline:string;
+  public prodCategory:string;
 
-}
+  
+ category:string[]=['Grocery','Mobile','Electronics','Cloths'];
+ 
+
+ saveForm(prodForm){
+   console.log("Product Form Data="+prodForm.value);
+ }
+
+
 }
 
 
